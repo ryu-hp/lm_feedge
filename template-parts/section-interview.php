a19d9f26f789f2905342af372011abd56336820f
ryu-hp <ryupon34689@gmail.com>
2025-07-20 06:30:49 +0900

    インタビューセクションの画像を動的に取得し、表示を最適化しました。また、FAQセクションのスタイルを整理しました。

--- template-parts/section-interview.php
+++ template-parts/section-interview.php
@@ -11,6 +11,7 @@
       <div id="interview-swiper" class="swiper interview-swiper">
         <div class="swiper-wrapper">
           <?php
+          // まず全インタビュー投稿を配列に格納
           $args = [
             'post_type'      => 'interview',
             'posts_per_page' => -1,
@@ -18,43 +19,42 @@
             'order'          => 'DESC',
           ];
           $interview_query = new WP_Query($args);
+          $interview_items = [];
           if ($interview_query->have_posts()):
             while ($interview_query->have_posts()): $interview_query->the_post();
               $name_initial = get_post_meta(get_the_ID(), 'interview_name_initial', true);
               $career      = get_post_meta(get_the_ID(), 'interview_career', true);
               $image_id    = get_post_meta(get_the_ID(), 'interview_image', true);
               $image_url   = $image_id ? wp_get_attachment_image_url($image_id, 'medium') : '';
+              $interview_items[] = [
+                'name_initial' => $name_initial,
+                'career'       => $career,
+                'image_url'    => $image_url,
+              ];
+            endwhile;
+            wp_reset_postdata();
+          endif;
+
+          // 2回繰り返して出力
+          for ($repeat = 0; $repeat < 2; $repeat++) {
+            foreach ($interview_items as $item):
           ?>
           <div class="swiper-slide">
             <div class="interview__item">
               <div class="interview__item-image">
-                <?php if ($image_url): ?>
-                  <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($name_initial); ?>">
+                <?php if (!empty($item['image_url'])): ?>
+                  <img src="<?php echo esc_url($item['image_url']); ?>" alt="<?php echo esc_attr($item['name_initial']); ?>">
                 <?php endif; ?>
               </div>
               <div class="interview__item-text">
-                <p class="interview__name"><?php echo esc_html($name_initial); ?></p>
-                <p class="interview__experience"><?php echo esc_html($career); ?></p>
-              </div>
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="interview__item">
-              <div class="interview__item-image">
-                <?php if ($image_url): ?>
-                  <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($name_initial); ?>">
-                <?php endif; ?>
-              </div>
-              <div class="interview__item-text">
-                <p class="interview__name"><?php echo esc_html($name_initial); ?></p>
-                <p class="interview__experience"><?php echo esc_html($career); ?></p>
+                <p class="interview__name"><?php echo esc_html($item['name_initial']); ?></p>
+                <p class="interview__experience"><?php echo esc_html($item['career']); ?></p>
               </div>
             </div>
           </div>
           <?php
-            endwhile;
-            wp_reset_postdata();
-          endif;
+            endforeach;
+          }
           ?>
         </div>
       </div>
