7b03eb45c44c447849a17d67032c87a900212bbb
ryu-hp <ryupon34689@gmail.com>
2025-07-20 06:57:24 +0900

    KVセクションのスタイルを最適化し、テキストグループを追加しました。また、パディングとフォントサイズを調整しました。

--- template-parts/section-kv.php
+++ template-parts/section-kv.php
@@ -1,8 +1,10 @@
-<section id="kv" class="section section--kv">
-  <div class="section__inner">
-    <div class="kv__text">
-      <img src="<?php echo get_template_directory_uri(); ?>/image/KV_bottom_text.svg" alt="powers reedge">
+<section id = "kv" class = "section section--kv">
+  <div class="section__inner section__inner--kv container">
+    <div class="kv__text-group">
+      <div class="kv__text">
+        <img src="<?php echo get_template_directory_uri(); ?>/image/KV_bottom_text.svg" alt="powers reedge">
+      </div>
+      <div class="kv__text--sub">あなたの自由と本気で向き合う会社</div>
     </div>
-    <div class="kv__text--sub">あなたの自由と本気で向き合う会社</div>
   </div>
 </section>
