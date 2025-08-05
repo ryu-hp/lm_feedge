47375c9ee4583c81ccad4ff2b7b286d0478df8fb
ryu-hp <ryupon34689@gmail.com>
2025-08-05 21:04:45 +0900

    Add simple blue arrow SVG icon

--- template-parts/section-kv.php
+++ template-parts/section-kv.php
@@ -2,7 +2,10 @@
   <div class="section__inner section__inner--kv inner">
     <div class="kv__text-group">
       <div class="kv__text js-fadeInUp">
-        <img src="<?php echo get_template_directory_uri(); ?>/image/KV_bottom_text.svg" alt="powers reedge">
+        <picture>
+          <source srcset="<?php echo get_template_directory_uri(); ?>/image/KV_bottom_text_sp.svg" media="(max-width: 767px)" type="image/png">
+          <img src="<?php echo get_template_directory_uri(); ?>/image/KV_bottom_text.png" alt="powers freedge">
+        </picture>
       </div>
       <div class="kv__text--sub js-fadeInUp">あなたの自由と本気で向き合う会社</div>
     </div>
