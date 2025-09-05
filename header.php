d079dedf20f42f09764648195f416c7872d99576
ryu-hp <ryupon34689@gmail.com>
2025-09-05 13:33:36 +0900

    Google Analyticsのスクリプトを追加

--- header.php
+++ header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SRFS3H1TJG"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SRFS3H1TJG');
+  </script>
+  <!-- End Google Analytics -->
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title> -->
